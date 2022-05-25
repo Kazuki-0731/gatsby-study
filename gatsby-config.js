dcc485eca505db9d62d088322e963f7ffb28a8ef
Kazuki-0731 <28224709+Kazuki-0731@users.noreply.github.com>
2022-05-25 20:41:42 +0900

    Fixed image path

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         "name": "images",
-        "path": "./src/images/"
+        "path": "./src/assets/images/"
       },
       __key: "images"
     }, {
