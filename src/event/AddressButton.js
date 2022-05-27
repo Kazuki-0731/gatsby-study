f37df636472a7f89be8fc674789dad001f1472e7
Kazuki-0731 <28224709+Kazuki-0731@users.noreply.github.com>
2022-05-28 02:00:57 +0900

    prettier formatted

--- src/event/AddressButton.js
+++ src/event/AddressButton.js
@@ -1,36 +1,33 @@
-
 import React from 'react'
 
 class AddressButton extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = { address : "住所未取得" };
-    this.handleClick = this.handleClick.bind(this);
+    super(props)
+    this.state = { address: '住所未取得' }
+    this.handleClick = this.handleClick.bind(this)
   }
 
   // This syntax ensures `this` is bound within handleClick.
   // Warning: this is *experimental* syntax.
   handleClick = () => {
     fetch('https://api.zipaddress.net/?zipcode=1000005', {
-      mode: 'cors'
+      mode: 'cors',
     })
-    .then(res => {
-      return res.json();
-    })
-    .then(json => {
-      this.setState({address:json.data.fullAddress});
-    })
+      .then((res) => {
+        return res.json()
+      })
+      .then((json) => {
+        this.setState({ address: json.data.fullAddress })
+      })
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.handleClick}>
-          Click me
-        </button>
+        <button onClick={this.handleClick}>Click me</button>
         <p>{this.state.address}</p>
       </div>
-    );
+    )
   }
 }
 
